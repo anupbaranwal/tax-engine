f30a4f2614732ed788699ea80664d9a386f970cd
Anup Kumar <anup.raj1989@gmail.com>
2019-03-17 15:51:49 +0530

    feat(initial-commit): Initial commit for tax engine

---
+++ src/test/java/com/taxathon/taxengine/TaxEngineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.taxathon.taxengine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaxEngineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
